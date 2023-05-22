b64c496497315674236e121c2803118925fa2300
Daniel Mateu <danielmateu86@gmail.com>
2023-05-22 11:32:33 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+
+  return (
+    <div className="text-center">
+      <h1 className='text-xl'>CleanCode y Solid</h1>
+      <p>Revisar la consola de JS</p>
+    </div>
+  )
+}
+
+export default App
